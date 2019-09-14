dfc258fc54db7e6ad586c39e9aa129b11a6a8948
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-14 21:20:41 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+katz_deli = []
+def line(array) 
+  if array == []
+    puts "The line is currently empty."
+  else
+    array.each { |name, index|
+      print "The line is currently " 
+      print "#{(index + 1)}. #{name}"
+    }
+  end
+end
+
+def take_a_number(array, name)
+  array << name
+  index = array.find_index(name)
+  puts "Welcome, #{name}. You are number #{++index} in line."
+end
+
+def now_serving(array)
+  if array == []
+    puts "There is nobody waiting to be servid!"
+  else
+    puts "Now serving #{array[0]}"
+    array.delete(array[0])
+  end
+end
+    
