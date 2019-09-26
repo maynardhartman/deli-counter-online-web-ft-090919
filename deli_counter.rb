960e3ffcee6118015da1a381cb785324293939ee
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-26 16:30:52 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,24 +4,25 @@ def line(array)
   if array == []
     puts "The line is currently empty."
   else
-    array.each { |name, index|
-      print "The line is currently " 
-      print "#{(index + 1)}. #{name}"
+    current_line = "The line is currently:"
+    array.each.with_index(1) { |name, index|
+      current_line << " #{index}. #{name}" 
     }
+    puts current_line
   end
 end
 
 def take_a_number(array, name)
   array << name
   index = array.find_index(name)
-  puts "Welcome, #{name}. You are number #{++index} in line."
+  puts "Welcome, #{name}. You are number #{index + 1} in line."
 end
 
 def now_serving(array)
   if array == []
-    puts "There is nobody waiting to be servid!"
+    puts "There is nobody waiting to be served!"
   else
-    puts "Now serving #{array[0]}"
+    puts "Currently serving #{array[0]}."
     array.delete(array[0])
   end
 end
